0be2488a9854b7c66ba3c84a2e225af868131e11
Brad Melin <melinbrad@gmail.com>
2017-08-28 16:33:36 +0200

    Rebase cloudera patches over current master

--- Pods/Target Support Files/Pods-edX/Pods-edX-resources.sh
+++ Pods/Target Support Files/Pods-edX/Pods-edX-resources.sh
@@ -23,6 +23,24 @@ case "${TARGETED_DEVICE_FAMILY}" in
     ;;
 esac
 
+case "${TARGETED_DEVICE_FAMILY}" in
+  1,2)
+    TARGET_DEVICE_ARGS="--target-device ipad --target-device iphone"
+    ;;
+  1)
+    TARGET_DEVICE_ARGS="--target-device iphone"
+    ;;
+  2)
+    TARGET_DEVICE_ARGS="--target-device ipad"
+    ;;
+  3)
+    TARGET_DEVICE_ARGS="--target-device tv"
+    ;;
+  *)
+    TARGET_DEVICE_ARGS="--target-device mac"
+    ;;
+esac
+
 install_resource()
 {
   if [[ "$1" = /* ]] ; then
